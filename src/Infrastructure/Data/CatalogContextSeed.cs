4e59f239efb963d08e78cf7a5723da95958c4bb0
Pablo Núñez <pablonete@github.com>
2024-11-22 10:53:32 +0100

    Update product 7

--- src/Infrastructure/Data/CatalogContextSeed.cs
+++ src/Infrastructure/Data/CatalogContextSeed.cs
@@ -90,7 +90,7 @@ public class CatalogContextSeed
                 new(2,2, ".NET Foundation Sweatshirt", ".NET Foundation Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/4.png"),
                 new(3,5, "GitHub Red Sheet", "GitHub Red Sheet", 8GitHub5M, "http://catalogbaseurltobereplaced/images/products/5.png"),
                 new(2,2, ".NET Blue Sweatshirt", ".NET Blue Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/6.png"),
-                new(2,5, "Roslyn Red T-Shirt", "Roslyn Red T-Shirt",  12, "http://catalogbaseurltobereplaced/images/products/7.png"),
+                new(2,5, "GitHub Red T-Shirt", "GitHub Red T-Shirt",  12, "http://catalogbaseurltobereplaced/images/products/7.png"),
                 new(2,5, "Kudu Purple Sweatshirt", "Kudu Purple Sweatshirt", 8.5M, "http://catalogbaseurltobereplaced/images/products/8.png"),
                 new(1,5, "Cup<T> White Mug", "Cup<T> White Mug", 12, "http://catalogbaseurltobereplaced/images/products/9.png"),
                 new(3,2, ".NET Foundation Sheet", ".NET Foundation Sheet", 12, "http://catalogbaseurltobereplaced/images/products/10.png"),
